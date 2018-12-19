315ce2594ba650d84d1cd0e1cbdd6beadb085df0
Nakya <31653682+tikusdugem@users.noreply.github.com>
2018-12-19 18:58:53 +0700

    Added tag <v-app>
    https://vuetifyjs.com/en/components/toolbars#examples
    
    On the example view source of App Bar, if I copy paste the code then color="primary" not working but if I added tag <v-app> like example in your codepen it works.

--- src/examples/toolbars/appBar.vue
+++ src/examples/toolbars/appBar.vue
@@ -1,25 +1,27 @@
 <template>
-  <v-toolbar dark color="primary">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
+  <v-app>
+    <v-toolbar dark color="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
 
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-icon>apps</v-icon>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-icon>refresh</v-icon>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-icon>more_vert</v-icon>
-    </v-btn>
-  </v-toolbar>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-app>
 </template>
